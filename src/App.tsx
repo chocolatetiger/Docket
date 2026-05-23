fa73e1fe0f996d8248e4bbb4d607315508941e85
Raghavendra Yellapragada <programmer.yellapragada@gmail.com>
2026-05-23 19:27:28 -0400

    chore: remove App.tsx and ignore Dockerfile.jenkins

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-const App = () => {
-  return (
-    <div className="min-h-screen bg-gray-950 text-white">
-      <h1 className="text-4xl font-bold text-center p-8">Docket</h1>
-    </div>
-  );
-};
-
-export { App };
