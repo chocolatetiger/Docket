014aac094279b42f7cb91c864228f2815b5bea32
Raghavendra Yellapragada <programmer.yellapragada@gmail.com>
2026-05-29 18:56:03 -0400

    feat: add TaskCard component with priority badge and due date

---
+++ src/features/tasks/components/TaskCard.tsx
@@ -0,0 +1,40 @@
+import type { Priority, Task } from '../../../types';
+import { capitalise, formatDate } from '../../../lib/utils';
+
+interface TaskCardProps {
+  task: Task;
+}
+const priorityStyles: Record<Priority, string> = {
+  high: 'bg-red-500/20 text-red-400 border border-red-500/30',
+  medium: 'bg-yellow-500/20 text-yellow-400 border border-yellow-500/30',
+  low: 'bg-green-500/20 text-green-400 border border-green-500/30',
+};
+
+const TaskCard = ({ task }: TaskCardProps) => {
+  return (
+    <article
+      className="bg-gray-900 
+          border border-gray-800 
+          rounded-xl p-5 
+          hover:bg-gray-800 
+          hover:border-gray-700 
+          hover:-translate-y-1 
+          hover:shadow-xl
+          transition-all duration-200
+          cursor-pointer"
+    >
+      <div className="text-white font-semibold text-lg">{task.title}</div>
+      <div className="text-gray-400 mt-2 text-lg">{task.description}</div>
+      <div
+        className={`text-xs px-2 py-1 rounded-full ${priorityStyles[task.priority]}`}
+      >
+        {capitalise(task.priority)}
+      </div>
+      <div className="text-gray-400 text-sm mt-2">
+        {task.dueDate ? formatDate(task.dueDate) : 'No date'}
+      </div>
+    </article>
+  );
+};
+
+export { TaskCard };
